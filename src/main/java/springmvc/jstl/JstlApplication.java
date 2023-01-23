d9be9406f01724a0f3529a6291018325da2da66e
Kim Hee ju <jju95@gmail.com>
2023-01-23 17:20:08 +0900

    2023-01-23 2

---
+++ src/main/java/springmvc/jstl/JstlApplication.java
@@ -0,0 +1,13 @@
+package springmvc.jstl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JstlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JstlApplication.class, args);
+	}
+
+}
